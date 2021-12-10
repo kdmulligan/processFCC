9ea424ce9a2bbcc3ea937f3f3c21625cc2828e0e
kdmulligan <kailey-mulligan@uiowa.edu>
2021-12-10 13:57:56 -0600

    add function into file

--- R/rollup_FCC.R
+++ R/rollup_FCC.R
@@ -64,3 +64,147 @@
 #' @importFrom data.table as.data.table fwrite
 
 
+process_fcc <- function(con,
+                        year,
+                        month,
+                        state = NULL,
+                        geogr = "cb",
+                        tech_exc = c("60", "70"),
+                        thresh_down = c(25, 25, 100),
+                        thresh_up = c(3, 5, 100)) {
+  if (!is.numeric(year))
+    stop("Year input should be a numeric")
+  if (!is.character(month))
+    stop("Month should be a character")
+  if (year < 2015 | year > 2020)
+    stop("Column names only provided for FCC data years 2015 through 2020")
+  if (!(month == "June" | month == "Dec"))
+    stop("Please use month equal to 'June' or 'Dec'")
+  if (!(length(thresh_down) == length(thresh_up)))
+    stop("Input upload and download speed threshold vectors should be of equal length")
+  if (!(is.numeric(thresh_down) & is.numeric(thresh_up)))
+    stop("Upload and download speed threshold vectors should be of type numeric")
+  if (!(geogr %in% c("cb", "cbg", "ct", "county")) || length(geogr) > 1)
+    stop("Input one of the `geogr` options: cb, cbg, ct, or county")
+  if (!all((tech_exc) %in% c(10, 11, 12, 20, 30, 40, 41, 42, 43, 60, 70, 80, 90, 0)))
+    stop("Input only tech code options")
+
+  table <- tbl(con, "table_fcc")
+  # create variable to subset census block by
+  stp <- if (geogr == "cb") {15
+  } else if (geogr == "cbg") {12
+  } else if (geogr == "ct") {11
+  } else if (geogr == "county") {5
+  }
+
+  #
+  table <- table %>%
+    mutate(cb_fips = as.character(as.integer(cb_fips))) %>%
+    # mutate(cb_fips = as.character((cb_fips))) %>%
+    mutate(cb_fips = ifelse(nchar(cb_fips) < 15, paste0("0", cb_fips), cb_fips)) %>%
+    mutate(cen_geo = substr(cb_fips, start = 1, stop = stp))
+
+  d1 = thresh_down[1]
+  d2 = thresh_down[2]
+  d3 = thresh_down[3]
+  d4 = thresh_down[4]
+  d5 = thresh_down[5]
+  u1 = thresh_up[1]
+  u2 = thresh_up[2]
+  u3 = thresh_up[3]
+  u4 = thresh_up[4]
+  u5 = thresh_up[5]
+  ## STEP 1: get list of unique cen_geo codes
+  ## STEP 2: add indicator variables to all at the census block level
+  if (is.null(state)) {
+    # get list of unique cen geo locations
+    geo_list <- table %>%
+      select(cen_geo, StateAbbr) %>%
+      distinct()
+    # step 1 of data processing NOT filtering state
+    dat_sub <- table %>%
+      filter(consumer == 1 & (!tech_code %in% tech_exc)) %>%
+      # filter(!tech_code %in% tech_exc) %>%
+      select(-c(1)) %>%
+      distinct() %>%
+      mutate(
+        speed1 = ifelse((max_down >= d1) & (max_up >= u1), 1, 0),
+        speed2 = ifelse((max_down >= d2) & (max_up >= u2), 1, 0),
+        speed3 = ifelse((max_down >= d3) & (max_up >= u3), 1, 0),
+        speed4 = ifelse((max_down >= d4) & (max_up >= u4), 1, 0),
+        speed5 = ifelse((max_down >= d5) & (max_up >= u5), 1, 0)
+      )
+  } else if (!is.null(state)) {
+    # get list of unique cen geo locations
+    geo_list <- table %>%
+      filter(StateAbbr %in% state) %>%
+      select(cen_geo, StateAbbr) %>%
+      distinct()
+    # step 1 of data processing filtering state
+    dat_sub <- table %>%
+      filter(StateAbbr %in% state) %>%
+      filter(consumer == 1 & (!tech_code %in% tech_exc)) %>%
+      # filter(!tech_code %in% tech_exc) %>%
+      select(-c(1)) %>%
+      distinct() %>%
+      mutate(
+        speed1 = ifelse((max_down >= d1) & (max_up >= u1), 1, 0),
+        speed2 = ifelse((max_down >= d2) & (max_up >= u2), 1, 0),
+        speed3 = ifelse((max_down >= d3) & (max_up >= u3), 1, 0),
+        speed4 = ifelse((max_down >= d4) & (max_up >= u4), 1, 0),
+        speed5 = ifelse((max_down >= d5) & (max_up >= u5), 1, 0)
+      )
+  }
+
+  ##  STEP 3 group by cen_geo and FRN (inidividual provider)
+  dat_grp_FRN <- dat_sub %>%
+    group_by(cen_geo, FRN) %>%
+    summarise(
+      speed1_uniq = max(speed1, na.rm = TRUE),
+      speed2_uniq = max(speed2, na.rm = TRUE) ,
+      speed3_uniq = max(speed3, na.rm = TRUE),
+      speed4_uniq = max(speed4, na.rm = TRUE),
+      speed5_uniq = max(speed5, na.rm = TRUE)
+    )
+  ## STEP 4 group by only cen_geo
+  dat_grp <- dat_grp_FRN %>%
+    group_by(cen_geo) %>%
+    summarise(
+      num_prov_1 = sum(speed1_uniq, na.rm = TRUE),
+      num_prov_2 = sum(speed2_uniq, na.rm = TRUE),
+      num_prov_3 = sum(speed3_uniq, na.rm = TRUE),
+      num_prov_4 = sum(speed4_uniq, na.rm = TRUE),
+      num_prov_5 = sum(speed5_uniq, na.rm = TRUE)
+    )
+
+  ## STEP 5 join processed data to geolist
+  final_data <- geo_list %>%
+    left_join(dat_grp, by = "cen_geo") %>%
+    replace_na(list(
+      num_prov_1 = 0,
+      num_prov_2 = 0 ,
+      num_prov_3 = 0,
+      num_prov_4 = 0,
+      num_prov_5 = 0
+    ))
+
+  new_col_names <- paste0("num_prov", thresh_down, "_", thresh_up)
+
+  output.dat <- as.data.table(final_data)
+  # rename columns of number of providers based on user input
+  output.dat <- output.dat %>%
+    rename_at(vars(starts_with("num_prov_")),
+              ~ c(paste0("num_prov", thresh_down, "_", thresh_up)))  %>%
+    rename_at(vars(starts_with("cen_geo")), ~ (geogr))
+
+  states_to_print <- ifelse(is.null(state), "all", state)
+  new_file <- paste0("fcc_processed_", month, year, ".csv")
+  print(cat("Your processed FCC dataset from", month, year,
+            "has", states_to_print, "states and is rolled up to the",
+            geogr, "level, excluding (", tech_exc, ") technology codes",
+            "and counts the number of the providers at the paired download,",
+            thresh_down, ", and upload,", thresh_up, ", speeds (Mbps).",
+            "This new file is saved in the working directory to", new_file, "   "))
+  fwrite(output.dat,
+         file = new_file)
+}
